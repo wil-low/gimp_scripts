4d6db437f27083410b7d97440a7ffb77c26c9c63
Andrei Ivushkin <willow@willow-dell>
2019-10-17 00:02:24 +0300

    Target image and group options

--- plugins/layered_guillotine.py
+++ plugins/layered_guillotine.py
@@ -3,7 +3,7 @@
 
 from gimpfu import *
 
-def layered_guillotine(image, prefix, use_layer_name_as_prefix, min_width, min_height):
+def layered_guillotine(image, prefix, use_prefix_as_group_name, append_layer_name_to_prefix, tgt_image, min_width, min_height):
   max_width = 0
   max_height = 0
 
@@ -71,16 +71,42 @@ def layered_guillotine(image, prefix, use_layer_name_as_prefix, min_width, min_h
   pdb.gimp_context_push()
   pdb.gimp_image_undo_group_start(image)
 
-  out_img = pdb.gimp_image_new(max_width, max_height, RGB)
+  images = gimp.image_list()
+  image_ids = pdb.gimp_image_list()[1]
 
-  if use_layer_name_as_prefix == 1:
+  out_img = -1
+
+  tgt_group = None
+  new_display = False
+
+  try:
+    i = image_ids.index(tgt_image)
+    out_img = images[i]
+    if use_prefix_as_group_name == 1:
+      for layer in out_img.layers:
+        if pdb.gimp_item_get_name(layer) == prefix:
+          if pdb.gimp_item_is_group(layer):
+            tgt_group = layer
+            break 
+  except ValueError:
+    out_img = pdb.gimp_image_new(max_width, max_height, RGB)
+    new_display = True
+
+  if tgt_group is None:
+    tgt_group = pdb.gimp_layer_group_new(out_img)
+    pdb.gimp_item_set_name(tgt_group, prefix)
+    pdb.gimp_image_insert_layer(out_img, tgt_group, None, 0)
+
+  if append_layer_name_to_prefix == 1:
     prefix += "_" + pdb.gimp_item_get_name(pdb.gimp_image_get_active_layer(image))
 
-  pdb.gimp_image_set_filename(out_img, "_LG_%s" % prefix)
+  total_ops = float(len(x_coords) * len(y_coords))
 
   draw0 = pdb.gimp_image_get_active_drawable(image)
-  for j in range(len(x_coords)):
-    for i in range(len(y_coords)):
+
+  counter = 0
+  for i in range(len(y_coords)):
+    for j in range(len(x_coords)):
       x = x_coords[j]
       y = y_coords[i]
 
@@ -89,18 +115,22 @@ def layered_guillotine(image, prefix, use_layer_name_as_prefix, min_width, min_h
 
       layer = pdb.gimp_layer_new(out_img, max_width, max_height, RGBA_IMAGE, "%s_%02d_%02d" % (prefix, i, j), 100, NORMAL_MODE)
 
-      pdb.gimp_image_insert_layer(out_img, layer, None, -1)
+      pdb.gimp_image_insert_layer(out_img, layer, tgt_group, -1)
       pdb.gimp_edit_clear(layer)
 
       pdb.gimp_image_set_active_layer(out_img, layer)
       pdb.gimp_edit_paste(layer, TRUE)
       layer = pdb.gimp_image_get_floating_sel(out_img)
       pdb.gimp_floating_sel_anchor(layer)
+      counter += 1
+      pdb.gimp_progress_update(counter / total_ops)
+
+  if new_display:
+    pdb.gimp_display_new(out_img)
 
   pdb.gimp_selection_none(image)
   pdb.gimp_selection_none(out_img)
 
-  pdb.gimp_display_new(out_img)
   pdb.gimp_displays_flush()
 
   pdb.gimp_image_undo_group_end(image)
@@ -117,8 +147,10 @@ register(
           "*",
           [
               (PF_IMAGE, "image", "Source image", None),
-              (PF_STRING, "prefix", "Layer's name prefix", "pr"),
-              (PF_TOGGLE, "use_layer_name_as_prefix", "Add layer name to prefix", 1),
+              (PF_STRING, "prefix", "Layer name prefix", "pr"),
+              (PF_TOGGLE, "use_prefix_as_group_name", "Use prefix as group name", 1),
+              (PF_TOGGLE, "append_layer_name_to_prefix", "Add layer name to prefix", 1),
+              (PF_INT, "tgt_image", "Target image ID", "-1"),
               (PF_INT, "min_width", "Minimal width", "100"),
               (PF_INT, "min_height",  "Minimal height", "100")
               
